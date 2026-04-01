aeeec1b8eb64252f1e9383ca27d3c02a2270cbba
Julien Desrosiers <juliend2@gmail.com>
2026-04-01 12:25:12 -0400

    fix config path

--- rp/callback-google.php
+++ rp/callback-google.php
@@ -4,6 +4,6 @@ ini_set('display_errors', '1');
 ini_set('log_errors_max_len', '-1');
 
 // google-specific config
-$conf = json_decode(file_get_contents(__DIR__."/../config.google.json"), true);
+$conf = json_decode(file_get_contents(__DIR__."/config.google.json"), true);
 
 include __DIR__ . '/auth-callback.php';
--- rp/callback-ms.php
+++ rp/callback-ms.php
@@ -5,6 +5,6 @@ ini_set('log_errors_max_len', '-1');
 
 
 // microsoft-specific config
-$conf = json_decode(file_get_contents(__DIR__."/../config.ms.json"), true);
+$conf = json_decode(file_get_contents(__DIR__."/config.ms.json"), true);
 
 include __DIR__ . '/auth-callback.php';
